ee32d505a1674f3995b43133d2eced9de323c6d1
Noah2211 <noah.decroix3@gmail.com>
2022-02-02 11:50:58 +0100

    done

--- classes/Personnage.php
+++ classes/Personnage.php
@@ -5,13 +5,13 @@ class Personnage {
 
     // La propriété id est privée, je ne peux pas la modifier ou la lire directement en dehors de cette classe
     private $id;
-    public $nom;
+    private $nom;
     public $x;
     public $y;
 
     // Constructeur de ma classe permettant de définir des valeurs par défaut lorsqu'elle est instanciée
-    public function __construct() {
-        $this->nom = "Nom par défaut";
+    public function __construct($nom = "Default") {
+        $this->setNom($nom);
         $this->x = 0;
         $this->y = 0;
     }
@@ -22,6 +22,34 @@ class Personnage {
 
     }
 
+    public function walkLeft() {
+        $this->x -= 1 ;
+    }
+
+    public function walkTop() {
+        $this->y += 1 ;
+    }
+
+    public function walkDown() {
+        $this->y -= 1;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
     // Setter permettant de définir l'attribut privé id
     public function setId($id) {
         $this->id = $id;
@@ -30,5 +58,21 @@ class Personnage {
     // Getter permettant de retourner l'attribut privé id
     public function getId() {
         return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * @return int
+     */
+    public function getY()
+    {
+        return $this->y;
     }
 } 
--- index.php
+++ index.php
@@ -5,19 +5,45 @@ include "classes/Personnage.php";
 // Creation d'une instance de la classe personnage
 $perso = new personnage();
 
-echo $perso->nom;
-
-echo"<br><br>";
+echo $perso->getNom();
 
 
+$perso->walkRight();
+$perso->walkRight();
+$perso->walkRight();
+$perso->walkRight();
 
+$perso->walkLeft();
+
+$perso->walkTop();
+$perso->walkTop();
+$perso->walkTop();
+$perso->walkTop();
+
+$perso->walkDown();
+
+
+echo "<br>Position X: ".$perso->getX();
+echo "<br>Position Y: ".$perso->getY();
+echo "<br><br>";
 $perso2 = new personnage();
-$perso2->nom = "Robert";
-echo $perso2->nom;
+$perso2->setNom("Custom name");
+echo $perso2->getNom();
 
 
+$perso2->walkRight();
+$perso2->walkRight();
 
-$perso->walkRight();
-$perso->walkRight();
-$perso->walkRight();
-$perso->walkRight();
+
+$perso2->walkLeft();
+
+$perso2->walkTop();
+$perso2->walkTop();
+$perso2->walkTop();
+
+$perso2->walkDown();
+
+echo "<br>Position X: ".$perso2->getX();
+echo "<br>Position Y: ".$perso2->getY();
+
+
